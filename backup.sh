b0fd3d0b1d4e38ce6c8ba12c3cc332bb65dc9554
Julien <julien@tvty.tv>
2017-05-18 12:45:45 +0200

    Fix uid and gid of generated file

--- backup.sh
+++ backup.sh
@@ -8,10 +8,16 @@ VOLUMES_TO_BACKUP=( $(mount | grep ext4 | grep -v /etc | grep -v ${BACKUP_TARGET
 
 TARGET=/backups/backup_${NOW}.tar.gz
 
+# Retrieve local user UID and group GID
+cd /backups/
+set -- `ls -nd .` && LOCAL_UID=$3 && LOCAL_GID=$4
 
 for VOLUME in "${VOLUMES_TO_BACKUP[@]}"
 do
+    printf "Backup volume : ${VOLUME} ... "
 	TARGET=/backups/backup_$(basename ${VOLUME})_${NOW}.tar.gz
    	cd $(dirname ${VOLUME})
-   	tar -czf ${TARGET} $(basename ${VOLUME})
+    tar -czf ${TARGET} $(basename ${VOLUME})
+    chown $LOCAL_UID:$LOCAL_GID ${TARGET}
+    printf "Done\n"
 done
